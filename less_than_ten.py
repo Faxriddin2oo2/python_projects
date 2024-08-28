7f701a4b86d877ca0803c7a08cb298ec97fd392e
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-08-28 17:34:54 +0500

    create less_than_ten.py

---
+++ less_than_ten.py
@@ -0,0 +1,20 @@
+# Bismillah
+"""
+Created on Wed Aug 28 17:20:50 2024
+
+@author: Faxriddin
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b=[]
+
+# for number in a:
+#     if number < 5:
+#         b.append(number)
+        
+# print(b)
+# print([num for num in a if num<5])
+number = int(input("Give a number: "))
+print([num for num in a if num<number])
+
+
