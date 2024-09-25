0addf4e5e37a4f2b9aa57e85ce3cedb21af992c3
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-25 23:01:38 +0500

    update caesar.py

--- ceiser_cipher.py
+++ ceiser_cipher.py
@@ -7,16 +7,23 @@ Created on Wed Sep 25 22:05:03 2024
 
 text = 'Assalomu alaykum'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+    
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char 
+        else:
+            index = alphabet.find(char)
+            # print(char)
+            # new_index = (index + shift)%26 # if didn't write %26 there, the function will seek 27, 28 letter and will give IndexErorr
+            new_index = (index + offset) % len(alphabet) # I did this to escape the counting mistakes
+            # encrypted_text = alphabet[new_index]
+            encrypted_text = encrypted_text + alphabet[new_index]   # In every cycle adding a letter to previous
+        # print('char:', char, ' encrypted_text:', encrypted_text)
+    
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char 
-    else:
-        index = alphabet.find(char)
-        # print(char)
-        new_index = (index + shift)%26
-        # encrypted_text = alphabet[new_index]
-        encrypted_text = encrypted_text + alphabet[new_index]   # In every cycle adding a letter to previous
-    print('char:', char, ' encrypted_text:', encrypted_text)
+caesar(text, shift)
