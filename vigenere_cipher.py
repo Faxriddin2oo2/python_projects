540f75bb39e5ad1389d4de99cd029b5940f7980b
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-26 20:13:23 +0500

    update vigenere_cipher.py

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -5,18 +5,53 @@ Created on Wed Sep 25 23:04:12 2024
 @author: Faxriddin
 """
 
-text = 'Assalamu alaykum'
-custom_key = 'python'
+# text = 'Assalamu alaykum'
+# custom_key = 'python'
 
-def vigenere(message, key, direction):
+# def vigenere(message, key, direction):
+#     key_index = 0
+#     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#     final_message = ''
+
+#     for char in message.lower():
+    
+#         # Append space to the message
+#         if char == ' ':
+#             final_message += char
+#         else:        
+#             # Find the right key character to encode/decode
+#             key_char = key[key_index % len(key)]
+#             key_index += 1
+
+#             # Define the offset and the encrypted/decrypted letter
+#             offset = alphabet.index(key_char)
+#             index = alphabet.find(char)
+#             new_index = (index + offset*direction) % len(alphabet)
+#             final_message += alphabet[new_index]
+    
+#     return final_message
+    
+# encryption = vigenere(text, custom_key, 1)
+# print(encryption)
+# decryption = vigenere(encryption, custom_key, -1)
+# print(decryption)
+
+
+
+
+# Updated version cipher above
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
+
+def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_message = ''
 
     for char in message.lower():
-    
-        # Append space to the message
-        if char == ' ':
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
             final_message += char
         else:        
             # Find the right key character to encode/decode
@@ -30,8 +65,14 @@ def vigenere(message, key, direction):
             final_message += alphabet[new_index]
     
     return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
     
-encryption = vigenere(text, custom_key, 1)
-print(encryption)
-decryption = vigenere(encryption, custom_key, -1)
-print(decryption)
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
