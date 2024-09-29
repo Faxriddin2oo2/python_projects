64d7fcdd3737c1784aa5982baf87bb610604c237
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-29 21:33:32 +0500

    Update password_generator.py
    Final product

--- password_generator.py
+++ password_generator.py
@@ -5,17 +5,95 @@ Created on Sun Sep 29 17:24:22 2024
 @author: Faxriddin
 """
 
+# import secrets
+# import string
+
+
+# # Define the possible characters for the password
+# letters = string.ascii_letters
+# digits = string.digits
+# symbols = string.punctuation
+
+# # Combine all characters
+# all_characters = letters + digits + symbols
+
+# print(all_characters)
+# print(secrets.choice(all_characters))
+
+# import re
+# import secrets
+# import string
+
+
+# def generate_password(length, nums, special_chars, uppercase, lowercase):
+#     # Define the possible characters for the password
+#     letters = string.ascii_letters
+#     digits = string.digits
+#     symbols = string.punctuation
+
+#     # Combine all characters
+#     all_characters = letters + digits + symbols
+
+#     while True:
+#         password = ''
+#         # Generate password
+#         for _ in range(length):
+#             password += secrets.choice(all_characters)
+        
+#         constraints = [
+#             (nums, r'\d'),
+#             (lowercase, r'[a-z]'),
+#             (uppercase, r'[A-Z]'),
+#             (special_chars, r'\W')
+#         ]        
+
+#     return password
+    
+# # new_password = generate_password(8)
+# # print(new_password)
+# pattern = r'\W'
+# quote = 'Not all those who wander are lost.'
+# print(re.findall(pattern, quote))
+
+
+# The final result
+import re
 import secrets
 import string
 
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
 
-# Combine all characters
-all_characters = letters + digits + symbols
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-# print(all_characters)
-# print(secrets.choice(all_characters))
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
+    return password
+    
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
