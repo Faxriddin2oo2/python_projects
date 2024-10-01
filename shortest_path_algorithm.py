946b160e5684fa62d4843a844f86c15cbba28606
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-01 21:50:49 +0500

    Update shortes_path_algorithm.py

--- shortest_path_algorithm.py
+++ shortest_path_algorithm.py
@@ -3,15 +3,39 @@
 Created on Tue Oct  1 21:22:35 2024
 
 @author: Faxriddin
+
+Project : Building a Shortest Path Algorithm
 """
 
-copper = {
-    'species': 'guinea pig',
-    'age': 2
-}
-copper['food'] = 'hay'
-copper['species'] = 'Cavia porcellus'
-del copper['age']
 
-for i, j in copper.items():
-    print(i, j)
+
+# copper = {
+#     'species': 'guinea pig',
+#     'age': 2
+# }
+# copper['food'] = 'hay'
+# copper['species'] = 'Cavia porcellus'
+# del copper['age']
+
+# for i, j in copper.items():
+#     print(i, j)
+
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+}
+
+def shortest_path(graph, start):
+    unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    
+shortest_path(my_graph, 'A')
