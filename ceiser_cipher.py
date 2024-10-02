a36513e5f9da72e71de93ab3f9ccbca8dfb13f4d
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-02 17:11:07 +0500

    Update ceiser_cipher.py
    Make some analyse and rewrote this cipher

--- ceiser_cipher.py
+++ ceiser_cipher.py
@@ -11,13 +11,15 @@ def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
     
-    for char in message.lower():
-        if char == ' ':
+    for char in message.lower(): # Making any message into lower letters
+        if char == ' ': # If char is empty then encrypted text also the same
             encrypted_text += char 
-        else:
-            index = alphabet.find(char)
+        else: 
+            index = alphabet.find(char) # 'index' will search the location of index of every single letter of word in alphabet list
             # print(char)
             # new_index = (index + shift)%26 # if didn't write %26 there, the function will seek 27, 28 letter and will give IndexErorr
+            
+            # Below we can see how we add some number to the current number of index, then we took result of this and every letter before will change to another
             new_index = (index + offset) % len(alphabet) # I did this to escape the counting mistakes
             # encrypted_text = alphabet[new_index]
             encrypted_text = encrypted_text + alphabet[new_index]   # In every cycle adding a letter to previous
@@ -26,5 +28,66 @@ def caesar(message, offset):
     print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
-caesar(text, shift)
-caesar(text, 9)
+# caesar(text, shift)
+# caesar(text, 9)
+
+
+
+import string
+
+letters = string.ascii_lowercase
+text = 'Hello world'
+shift = 4
+
+
+def ceasar_cipher(text, offset):
+    encrypted_text = ''
+    for char in text:
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = letters.find(char)
+            new_index = (index+offset)%len(letters)
+            encrypted_text += letters[new_index]
+        
+    print('Input: ', text)
+    print('Output: ', encrypted_text)
+
+
+ceasar_cipher(text, shift)
+ceasar_cipher('bir bir', 3)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
