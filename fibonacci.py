6a77aba480578ab8de8a7a3b5ed79fb7632b2bf4
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-09 22:28:41 +0500

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+# Bismillah
+"""
+Created on Wed Oct  9 22:11:41 2024
+
+@author: Faxriddin
+"""
+
+def fibonacci(count):
+    if count <= 0:
+        return []
+    if count == 1:
+        return [0]
+    if count == 2:
+        return [0,1]
+    
+    fib_sequence = [0,1]
+    for n in range(2, count):
+        fib_sequence.append(fib_sequence[n-1]+fib_sequence[n-2])
+        
+    return fib_sequence
+
+print(fibonacci(4))
