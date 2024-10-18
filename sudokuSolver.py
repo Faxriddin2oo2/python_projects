603ccf7b1ff48d8598c6fecbe97f95e7ee3a6984
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-18 21:56:38 +0500

    Create sudokuSolver.py
    It's only start

---
+++ sudokuSolver.py
@@ -0,0 +1,13 @@
+# Bismillah
+"""
+Created on Fri Oct 18 21:55:33 2024
+
+@author: Faxriddin
+"""
+
+class Board:
+    def spam(self):
+        print('Spam!')
+    
+gameboard = Board()
+gameboard.spam()
