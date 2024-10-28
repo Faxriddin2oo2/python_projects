808f7b36aeb96417d9efc2f371b1026012691ed1
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-28 20:19:18 +0500

    create decodeWebPage.py

---
+++ decodeWebPage.py
@@ -0,0 +1,22 @@
+# Bismillah
+"""
+Created on Mon Oct 28 20:13:39 2024
+
+@author: Faxriddin
+"""
+
+import requests
+from bs4 import BeautifulSoup
+ 
+base_url = 'http://www.nytimes.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, features="lxml")
+ 
+
+for story_heading in soup.find_all(class_="story-heading"): 
+    if story_heading.a: 
+        print(story_heading.a.text.replace("\n", " ").strip())
+    else: 
+        print(story_heading.contents[0].strip())
+        
+
