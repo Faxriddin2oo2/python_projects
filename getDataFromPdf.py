f1d34deba51413bca1ad6d22cd9727a88c20e003
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-10-29 23:50:47 +0500

    Update getDataFromPdf

--- getDataFromPdf.py
+++ getDataFromPdf.py
@@ -51,3 +51,65 @@ writeToJson(structure, json_file)
 
 
 
+
+
+
+
+
+
+
+
+# import fitz
+# import json
+
+def getDataFromPdf(path_to_pdf):
+    pdf = fitz.open(path_to_pdf)
+    content = pdf.get_toc() # Служит для извлечения оглавлений
+    
+    data = {} # Основной словарь
+    chapter_counter = 0
+    section_counter = 0
+
+    for i in content:
+        level, name, page = i
+        
+        if level == 1:
+            chapter_counter += 1
+            section_counter = 0
+            data[str(chapter_counter)] = {
+                "title": name,
+                "sections": {}
+            }
+        
+        elif level == 2:
+            section_counter += 1
+            data[str(chapter_counter)]["sections"][str(section_counter)] = {
+                "title": name,
+                "subsections": {}
+            }
+        
+        elif level == 3:
+            data[str(chapter_counter)]["sections"][str(section_counter)]["subsections"][name] = {
+                "title": name,
+                "page": page
+            }
+    
+    return data
+
+def writeToJson(struct, json_file):
+    f = open(json_file, 'w')
+    json.dump(struct, f, ensure_ascii=False, indent=4)
+    f.close()
+
+
+
+# Example
+file_pdf = "file.pdf"
+json_file = "structure.json"
+
+structure = getDataFromPdf(file_pdf)
+writeToJson(structure, json_file)
+
+
+
+
